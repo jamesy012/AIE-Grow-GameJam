9822d350cd27933cfab8babc65cc69b39c37edf5
AIE\s171728 <vince.chin.aie@gmail.com>
2017-06-28 17:07:39 +1000

    vine grows leaves with anim curves

--- Assets/Scripts/Leaf.cs
+++ Assets/Scripts/Leaf.cs
@@ -5,22 +5,44 @@ using UnityEngine;
 public class Leaf : MonoBehaviour
 {
     public float m_lifeTime = 10.0f;
+    public float m_startScale = 0.1f;
+    public float m_endScale = 5.0f;
+    public Vector3 m_scaleVector = Vector3.one;
+    public AnimationCurve m_curve;
+
     private float m_timer = 0.0f;
+    private float m_scaleDiff = 0.0f;
+    private float m_scaleInterval = 0.0f;
+    private float m_currScale = 0.0f;
+    private float m_ratio = 0.0f;
+
     // Use this for initialization
     void Start()
     {
+        m_ratio = 0.0f;
+        m_scaleDiff = m_endScale - m_startScale;
+        m_scaleInterval = m_scaleDiff / m_lifeTime;
 
+        this.transform.localScale = m_scaleVector * m_startScale;
+        m_currScale = m_startScale;
     }
 
     // Update is called once per frame
     void Update()
     {
         m_timer += Time.deltaTime;
+        m_ratio = m_timer / m_lifeTime;
+        m_currScale = m_curve.Evaluate(m_ratio) * m_endScale;
+
+        this.transform.localScale = m_scaleVector * m_currScale;
+
 
         if(m_timer >= m_lifeTime)
         {
 
             m_timer = 0.0f;
+            m_currScale = m_startScale;
+            this.transform.localScale = m_scaleVector * m_startScale;
             this.gameObject.SetActive(false);
         }
     }
