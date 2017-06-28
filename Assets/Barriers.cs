08bb5032fd770ac211fa7137aacdba26ba87e871
AIE\s171728 <vince.chin.aie@gmail.com>
2017-06-29 09:28:01 +1000

    added barriers to stop player from moving sideways forever

---
+++ Assets/Barriers.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barriers : MonoBehaviour
+{
+    private Player m_player;
+    // Use this for initialization
+    void Start()
+    {
+        m_player = GameObject.FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_player != null)
+        {
+            this.transform.position  = new Vector3(0,m_player.transform.position.y,0);
+        }
+    }
+}
