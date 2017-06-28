1963eb4a92072b1c057d2471a4402c068dabaaf7
AIE\s171728 <vince.chin.aie@gmail.com>
2017-06-28 15:22:27 +1000

    Player controls

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float m_moveSpeed = 10.0f;
+
+
+    private Vector3 m_moveDir = Vector3.zero;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Vector2 mousePos = Input.mousePosition;
+
+            Vector2 currPos = Camera.main.WorldToScreenPoint(this.transform.position);
+
+            m_moveDir = mousePos - currPos;
+
+            float distanceSqrd = m_moveDir.sqrMagnitude;
+
+
+            if (distanceSqrd > m_moveSpeed*2)
+            {
+                this.transform.position += m_moveDir.normalized * m_moveSpeed * Time.deltaTime;
+            }
+
+        }
+    }
+}
