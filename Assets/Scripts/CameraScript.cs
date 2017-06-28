866bf88c2d2ac992cc20cbf8cdeed0f8dd4bd892
James Owen <jamesowen012@gmail.com>
2017-06-28 16:01:30 +1000

    Added Camera script and turned camera into prefab added script to camera and saved scene

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public enum CameraModes {
+    Debug,
+    DontMove,
+    SideMove
+}
+
+public class CameraScript : MonoBehaviour {
+
+    public CameraModes m_CameraMode;
+
+    private Transform m_PlayerTransform;
+
+    [Header("a")]
+    [Range(0.0f,1.0f)]
+    public float m_VerticalRatio = 0.25f;
+
+    // Use this for initialization
+    void Start() {
+        Player player = FindObjectOfType<Player>();
+
+        if(player == null) {
+            Debug.LogError(GetType().ToString() + ": " + transform.name + " cant find Player script");
+            return;
+        }
+
+        m_PlayerTransform = player.transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate() {
+        moveCamera();
+    }
+
+    private void moveCamera() {
+        switch (m_CameraMode) {
+            case CameraModes.Debug:
+                cameraDebug();
+                break;
+            case CameraModes.SideMove:
+                cameraDebug();
+                break;
+            case CameraModes.DontMove:
+            default:
+                break;
+        }
+    }
+
+    private void cameraDebug() {
+        Vector3 pos = transform.position;
+        pos.x = m_PlayerTransform.position.x;
+        pos.y = m_PlayerTransform.position.y;
+
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/Spawners.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawners : MonoBehaviour {
+
+    /// <summary>
+    /// dds
+    /// </summary>
+    [Tooltip("Width between this point and where it's spawning stuff")]
+    public float m_Width = 5.0f;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+
+        Vector3 offset = (transform.rotation * new Vector3(m_Width, 0));
+        const float width = 0.5f;
+
+        Gizmos.DrawSphere(transform.position + offset, width);
+        Gizmos.DrawSphere(transform.position - offset, width);
+    }
+
+}
