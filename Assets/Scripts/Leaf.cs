4080dae6d870e3e9ac9a9fd2040fc14e00751bf7
AIE\s171728 <vince.chin.aie@gmail.com>
2017-06-28 15:59:23 +1000

    made leaf prefab

---
+++ Assets/Scripts/Leaf.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leaf : MonoBehaviour
+{
+    public float m_lifeTime = 10.0f;
+    private float m_timer = 0.0f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_timer += Time.deltaTime;
+
+        if(m_timer >= m_lifeTime)
+        {
+
+            m_timer = 0.0f;
+            this.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Leaves.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leaves : MonoBehaviour
+{
+    public GameObject m_leafPrefab;
+    public int m_poolAmount = 50;
+    public float m_leafHeightInterval = 10.0f;
+
+
+    private float m_startingHeight = 0.0f;
+    private float m_currHeight = 0.0f;
+    private List<GameObject> m_leaves = new List<GameObject>();
+    // Use this for initialization
+    void Start()
+    {
+        m_startingHeight = this.transform.position.y;
+        m_currHeight = m_startingHeight;
+        if (m_leafPrefab != null)
+        {
+            for(int i = 0; i< m_poolAmount; ++i)
+            {
+                GameObject leaf = GameObject.Instantiate(m_leafPrefab);
+                leaf.SetActive(false);
+                m_leaves.Add(leaf);
+            }
+        }
+    }
+
+    void SpawnLeaf()
+    {
+        for (int i = 0; i < m_poolAmount; ++i)
+        {
+            if(!m_leaves[i].activeInHierarchy)
+            {
+                m_leaves[i].transform.position = this.transform.position;
+                m_leaves[i].SetActive(true);
+            }
+        }
+    }
+
+
+    void Update()
+    {
+        m_currHeight = this.transform.position.y;
+
+        if(m_currHeight % m_leafHeightInterval < 0.01f)
+        {
+            SpawnLeaf();
+        }
+
+    }
+}
