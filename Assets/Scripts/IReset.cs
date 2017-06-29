949039c6844a6d3a4a0234f94faef2e5708c426b
James Owen <jamesowen012@gmail.com>
2017-06-29 14:15:29 +1000

    player death scripts and menu assets nothing changed

---
+++ Assets/Scripts/IReset.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class IReset : MonoBehaviour {
+
+    private Vector3 m_StartPos;
+
+    void Awake() {
+        m_StartPos = transform.position;
+    }
+
+    virtual public void reset() {
+        transform.position = m_StartPos;
+    }
+
+}
---
+++ Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour {
+
+    private Transform m_PlayerTransform;
+    private Retry m_Retry;
+
+	// Use this for initialization
+	void Start () {
+        Player player = FindObjectOfType<Player>();
+        if (player == null) {
+            Debug.LogError(GetType().ToString() + ": " + transform.name + " cant find Player script");
+            return;
+        }
+        m_PlayerTransform = player.transform;
+
+        m_Retry = FindObjectOfType<Retry>();
+        if (m_Retry == null) {
+            Debug.LogError(GetType().ToString() + ": " + transform.name + " cant find Retry script");
+            return;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 playerScreenPos = Camera.main.WorldToScreenPoint(m_PlayerTransform.position);
+
+        if(playerScreenPos.y <= 0) {
+            m_Retry.showScreen();
+        }
+	}
+}
