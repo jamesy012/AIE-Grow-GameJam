b605e718b7fd0f99cd94ef1ecdf3a32202b21b37
AIE\s171728 <vince.chin.aie@gmail.com>
2017-06-29 14:48:13 +1000

    updated prefabs to  have destroy on become invisible

---
+++ Assets/Scripts/DestroyWhenInvisible.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWhenInvisible : MonoBehaviour
+{
+
+    void OnBecameInvisible()
+    {
+        GameObject.Destroy(this.gameObject);
+    }
+}
