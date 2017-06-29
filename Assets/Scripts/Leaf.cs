4dddbe52fca3fff10d23057b7576dedf831a60d9
AIE\s171728 <vince.chin.aie@gmail.com>
2017-06-29 14:56:06 +1000

    update leaf prefab

--- Assets/Scripts/Leaf.cs
+++ Assets/Scripts/Leaf.cs
@@ -33,8 +33,8 @@ public class Leaf : MonoBehaviour
     {
          m_randNum = Random.Range(0, 2);
 
-
-
+        m_timer = 0.0f;
+        this.transform.localScale = m_scaleVector * m_startScale;
     }
 
     // Update is called once per frame
